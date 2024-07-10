d57967a8441633fae142440b16d4ab2ce47db907
lorena0292 <lorena0292@gmail.com>
2024-07-10 20:18:58 +0200

    conectando bdd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -50,6 +53,22 @@ android {
 }
 
 dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Declare the dependency for the Cloud Firestore library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-firestore")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -69,6 +88,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
     implementation(libs.material)
+    implementation(libs.firebase.database.ktx)
     // implementation(libs.androidx.foundation.layout.android)
 
     testImplementation(libs.junit)
--- app/src/main/java/com/example/beaEstilsApp/NuevaCita.kt
+++ app/src/main/java/com/example/beaEstilsApp/NuevaCita.kt
@@ -1,19 +1,36 @@
 package com.example.beaEstilsApp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.slider.RangeSlider
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+
 
 class NuevaCita : AppCompatActivity() {
 
     //Elementos
     lateinit var rngTiempo: RangeSlider
     lateinit var txtHoras: TextView
-    private var tiempo:Int=0
+    lateinit var cbMechas: CheckBox
+    lateinit var cbTinte: CheckBox
+    lateinit var cbCortar: CheckBox
+    lateinit var cbAlisado: CheckBox
+    lateinit var cbTratamiento: CheckBox
+    lateinit var cbHombre: CheckBox
+    lateinit var cbLavarMarcar: CheckBox
+    lateinit var cbExtraCorto: CheckBox
+    lateinit var cbExtraLargo: CheckBox
+
+    private var tiempo: Int = 0
+
+    // Access a Cloud Firestore instance from your Activity
+    val db = Firebase.firestore
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,26 +41,56 @@ class NuevaCita : AppCompatActivity() {
 
         initComponents()
         initListeners()
+        leeColeccion()
     }
 
 
     private fun initComponents() {
         rngTiempo = findViewById(R.id.rngTiempo)
-        txtHoras=findViewById(R.id.txtTiempo)
-        txtHoras.text="${tiempo.toString()} horas"
+        txtHoras = findViewById(R.id.txtTiempo)
+        txtHoras.text = "${tiempo.toString()} horas"
+        cbMechas = findViewById(R.id.cbMechas)
+        cbTinte = findViewById(R.id.cbTinte)
+        cbCortar = findViewById(R.id.cbCortar)
+        cbAlisado = findViewById(R.id.cbAlisado)
+        cbTratamiento = findViewById(R.id.cbTratamiento)
+        cbLavarMarcar = findViewById(R.id.cbLavarMarcar)
+        cbHombre = findViewById(R.id.cbHombre)
+        cbExtraCorto = findViewById(R.id.cbExtraCorto)
+        cbExtraLargo = findViewById(R.id.cbExtraLargo)
     }
 
     private fun initListeners() {
 //RangeSlider Tiempo Listener
-        rngTiempo.addOnChangeListener{slider, value, fromUser ->
-            tiempo=value.toInt()
-            if(tiempo==0){
+        rngTiempo.addOnChangeListener { slider, value, fromUser ->
+            tiempo = value.toInt()
+            if (tiempo == 0) {
                 txtHoras.text = "Menos de 1 hora"
-            }
-            else {
+            } else {
                 txtHoras.text = " ${tiempo.toString()} horas"
             }
         }
 
+    }
+
+    public fun leeColeccion() {
+        db.collection("servicios")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("", "${document.id} => ${document.data.getValue("minutos")}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("TAG", "Error getting documents.", exception)
+            }
+    }
+
+    public fun calculaTiempo() {
+        cbMechas.setOnCheckedChangeListener { _, isChecked ->
+            // if (isChecked) tiempo+=
+
+
+        }
     }
 }
